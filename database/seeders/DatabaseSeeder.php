94cac9fece89a8169b4345533a22eab868be923e
Miha Medven <3627889+aweCodeMan@users.noreply.github.com>
2021-08-13 10:40:32 +0200

    Add database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Aggregates\UserAggregate;
+use App\Models\Group;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +19,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        Group::create([
+            'uuid' => Str::orderedUuid(),
+            'name' => 'programiranje',
+            'slug' => 'programiranje',
+            'color' => '#63c7ff',
+            'order' => 0,
+        ]);
+
+        Group::create([
+            'uuid' => Str::orderedUuid(),
+            'name' => 'razno',
+            'slug' => 'razno',
+            'color' => '#FF9314',
+            'order' => 1,
+        ]);
     }
 }
