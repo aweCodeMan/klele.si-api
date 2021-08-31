c120e8131c7f95c0e3900c8ecc60e7c75d43041b
Miha Medven <3627889+aweCodeMan@users.noreply.github.com>
2021-08-31 13:51:13 +0200

    Add error emails

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,11 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\ErrorHandler\ErrorRenderer\HtmlErrorRenderer;
+use Symfony\Component\ErrorHandler\Exception\FlattenException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -35,7 +39,40 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
+            if ($this->shouldReport($e) && $this->shouldSendEmail($e)) {
+                $this->sendEmail($e);
+            }
         });
+    }
+
+    private function shouldSendEmail(Throwable $e)
+    {
+        return !config('app.debug');
+    }
+
+    private function sendEmail(Throwable $e)
+    {
+        try {
+            $request = $this->container->get(Request::class);
+            $user = $request->user();
+            $url = $request->fullUrl();
+            $flatten = FlattenException::createFromThrowable($e);
+            $handler = new HtmlErrorRenderer(true);
+            $css = $handler->getStylesheet();
+            $content = $handler->getBody($flatten);
+
+            Mail::send('emails.exception', compact('css', 'content'), function ($message) use ($url, $user) {
+                $subject = 'Exception: ' . $url;
+
+                if ($user) {
+                    $email = $user->email;
+                    $subject .= " ($email)";
+                }
+
+                $message->to(['miha@klele.si'])
+                    ->subject($subject);
+            });
+        } catch (Throwable $throwable) {
+        }
     }
 }
