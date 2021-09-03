851c12483941f3438eb9043b50f347a1905e7cd2
Miha Medven <3627889+aweCodeMan@users.noreply.github.com>
2021-09-03 22:51:40 +0200

    Make title nullable

--- app/Services/LinkData.php
+++ app/Services/LinkData.php
@@ -6,7 +6,7 @@ namespace App\Services;
 
 final class LinkData
 {
-    public function __construct(public string $title, public array $meta, public array $openGraph, public array $twitterCard)
+    public function __construct(public ?string $title, public array $meta, public array $openGraph, public array $twitterCard)
     {
     }
 }
